6aece6eaf78edf67ca41df5689a5789a50c604f7
Akadar.akku <akadar.akku121@gmail.com>
2020-02-26 11:32:14 +0100

    Updates

---
+++ precache-manifest.1202163da7b9667d4c93044be7f9ed44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2a883b8086c9dc491a6028cd79685d1",
+    "url": "/product-app/index.html"
+  },
+  {
+    "revision": "bf318b843a432c6632e8",
+    "url": "/product-app/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "6e3391772d66f7a9c542",
+    "url": "/product-app/static/js/2.cc45a804.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/product-app/static/js/2.cc45a804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf318b843a432c6632e8",
+    "url": "/product-app/static/js/main.75ea5047.chunk.js"
+  },
+  {
+    "revision": "5c95d024ab8113392c8d",
+    "url": "/product-app/static/js/runtime-main.a90a47e8.js"
+  }
+]);
